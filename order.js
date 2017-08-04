bf68f523c979aca2a8bd964100118c35a1a3113c
DaisyGuan <yguan11@jhu.edu>
2017-08-04 10:02:25 -0700

    Create order.js

---
+++ order.js
@@ -0,0 +1,23 @@
+var orderCount = 0;
+function takeOrder(topping, crustType){
+  orderCount = orderCount + 1;
+  console.log('Order: '+ crustType + ' crust pizza topped with ' + topping);
+}
+
+function getSubTotal(itemCount){
+  return itemCount * 7.5;
+};
+
+function getTax() {
+  return getSubTotal(orderCount) * 0.06;
+}
+
+function getTotal(){
+  return getSubTotal(orderCount) + getTax();
+}
+
+takeOrder('bacon', 'thin');
+takeOrder('ham', 'thin');
+takeOrder('bacon', 'thick');
+
+console.log(getTotal());
